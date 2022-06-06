de8ce0675e3954cc5d2636c341fcf9523c0a8be8
rolfmadsen <96595+rolfmadsen@users.noreply.github.com>
2022-06-06 12:20:01 +0200

    Moved Structs for opensearch to different file, to be imported as mod in main.rs

---
+++ opensearch/Archive/output_to_terminal/query_working_backup.rs
@@ -0,0 +1,26 @@
+use serde::Deserialize;
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+
+#[derive(Deserialize, Debug)]
+pub struct Query {
+    query_value: String,
+}
+
+pub fn read_query_from_file<P: AsRef<Path>>(path: P) -> Result<Query, Box<dyn Error>> {
+    let file = File::open(path).expect("Failed to open query.json");   // Open the file in read-only mode with buffer.
+    let reader = BufReader::new(file);    
+    let u = serde_json::from_reader(reader)?;   // Read the JSON contents of the file as an instance of `Query`.
+    Ok(u)   // Return the `Query`.
+}
+
+pub fn query_string() -> std::string::String {
+    let u = read_query_from_file("src/query.json").unwrap();
+    let return_value = format!("{:}", u.query_value); // NB. Use "let return_value = format!("{:}", u.query_value);" to escape single quotes in search query.
+    //println!("{:?}", return_value);
+    return return_value
+}
+
+// Source: https://docs.serde.rs/serde_json/fn.from_reader.html
